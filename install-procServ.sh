19df4eda5dfe098e9e01e17f4074b7a38e22e61f
Basil Aljamal <122060486+baljamal@users.noreply.github.com>
2024-02-20 13:35:31 -0800

    Update install-procServ.sh

--- install-procServ.sh
+++ install-procServ.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-# Check if the script is being run as root
-if [ "$(id -u)" -ne 0 ]; then
-    # If not root, prompt for sudo privileges and re-run the script
-    exec sudo "$0" "$@"
-fi
+doas pkg install -y autoconf automake lang/gcc gmake git
 
-pkg install -y autoconf automake lang/gcc gmake git
-
-git clone https://github.com/ralphlange/procServ.git
+# git clone https://github.com/ralphlange/procServ.git
 
 cd procServ
-autoconf
 gmake
-./configure --disable-doc
+#autoconf
+./configure --disable-doc --enable-access-from-anywhere
 git apply ../makefile.patch
 make
